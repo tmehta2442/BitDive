6863baa51ecd88f47f4d58e0f489625f887c6be9
tmehta2442 <tejas@bootinguptj.com>
2013-11-20 11:31:31 -0800

    BitDive's first Dive

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitdive::Application.config.secret_token = '4264c7d507532eae8b4ca039483c81475ae2d45d83350021b253a4b891ac5f5b574ae3773a493785436eff9d02eac0320647f81e99e43dbdb8cb9451521c0051'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Bitdive::Application.routes.draw do
+  get '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match'/auth/failure', :to => 'sessions#failure'
+end
