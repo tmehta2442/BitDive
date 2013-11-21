f9dd39f8be80c4165edc8ea0eba189e7421ddf9e
elyrly <elyrly@gmail.com>
2013-11-20 18:24:49 -0800

    changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   	@authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
   	if @authorization
   		render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
-  	else 
+  	else
   		user = User.create :name => auth_hash["info"]["name"], :email =>auth_hash["info"]["email"]
   		user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
   		user.save
@@ -22,10 +22,5 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	# render :text => "#{user.name}, you've logged out!"
-  	# render :text => "#{user.name}, you've logged out!"
-
-  	session[:user_id] = nil
-  	render :text => "You've logged out!"
   end
 end
